9a572a278af57d5c0ebb74d13850677c967e09e3
VictoriaBozzola <mvbozzola@gmail.com>
2023-04-04 19:29:09 -0300

    primer configuración

---
+++ Backend-tecnica/src/main/java/com/example/Backendtecnica/BackendTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backendtecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTecnicaApplication.class, args);
+	}
+
+}
---
+++ Backend-tecnica/src/test/java/com/example/Backendtecnica/BackendTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backendtecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
